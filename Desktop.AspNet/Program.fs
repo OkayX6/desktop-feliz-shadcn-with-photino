85c4c06cd98a8f9c5e430cddd4e35d2a65a967e2
Denis Ok <denis@getdefacto.com>
2025-08-19 00:28:30 +0800

    Fix project

--- Desktop.AspNet/Program.fs
+++ Desktop.AspNet/Program.fs
@@ -12,83 +12,81 @@ open Fable.Remoting.AspNetCore
 open Shared
 
 module Program =
-    open PhotinoNET
-    open Fable.Remoting.Server
-    open System.Net.Sockets
-    open System.Net
+  open PhotinoNET
+  open Fable.Remoting.Server
+  open System.Net.Sockets
+  open System.Net
 
-    let serverApi: IServerApi =
-        let mutable count = 0
+  let serverApi: IServerApi =
+    let mutable count = 0
 
-        { Counter = fun () -> async { return { Value = 0 } }
+    { Counter = fun () -> async { return count }
 
-          SystemInfo =
-              fun () ->
-                  async {
-                      return
-                          { Platform = Environment.OSVersion.Platform.ToString()
-                            Version = Environment.OSVersion.VersionString }
-                  }
+      SystemInfo =
+        fun () ->
+          async {
+            return
+              { Platform = Environment.OSVersion.Platform.ToString()
+                Version = Environment.OSVersion.VersionString }
+          }
 
-          Update =
-              fun () ->
-                  async {
-                      count <- count + 1
-                      return count
-                  } }
+      Update =
+        fun () ->
+          async {
+            count <- count + 1
+            return ()
+          } }
 
-    let exitCode = 0
+  let exitCode = 0
 
 
-    let isDevelopment =
+  let isDevelopment =
 #if DEBUG
-        true
+    true
 #else
-        false
+    false
 #endif
 
-    let randomPort =
-        let tcpListener = new TcpListener(IPAddress.Loopback, 0)
-        tcpListener.Start()
-        let port = (tcpListener.LocalEndpoint :?> IPEndPoint).Port
-        tcpListener.Stop()
-        port
+  let randomPort =
+    let tcpListener = new TcpListener(IPAddress.Loopback, 0)
+    tcpListener.Start()
+    let port = (tcpListener.LocalEndpoint :?> IPEndPoint).Port
+    tcpListener.Stop()
+    port
 
-    let desktopUrl =
-        if isDevelopment
-        // during development assume vite dev server is running
-        then
-            "http://localhost:5173"
-        // in release mode we run Suave on a random port
-        // which will host the static files generated
-        else
-            $"http://localhost:{randomPort}/index.html"
+  let desktopUrl =
+    if
+      isDevelopment
+    // during development assume vite dev server is running
+    then
+      "http://localhost:5173"
+    // in release mode we run Suave on a random port
+    // which will host the static files generated
+    else
+      $"http://localhost:{randomPort}/index.html"
 
-    let webApp =
-        Remoting.createApi ()
-        |> Remoting.fromValue serverApi
-        // It's not in the documentation but if you don't do it, it doesn't work
-        |> Remoting.withRouteBuilder routerPaths
+  let webApp =
+    Remoting.createApi ()
+    |> Remoting.fromValue serverApi
+    // It's not in the documentation but if you don't do it, it doesn't work
+    |> Remoting.withRouteBuilder routerPaths
 
-    [<EntryPoint; STAThread>]
-    let main args =
-        let builder = WebApplication.CreateBuilder(args)
+  [<EntryPoint; STAThread>]
+  let main args =
+    let builder = WebApplication.CreateBuilder(args)
 
-        // Configure the server to run on port 5000
-        builder.WebHost.UseUrls("http://localhost:5000")
+    // Configure the server to run on port 5000
+    builder.WebHost.UseUrls("http://localhost:5000")
 
-        let app = builder.Build()
+    let app = builder.Build()
 
-        // Add Remoting handler to the ASP.NET Core pipeline
-        app.UseRemoting(webApp)
+    // Add Remoting handler to the ASP.NET Core pipeline
+    app.UseRemoting(webApp)
 
-        Task.Run(fun () -> app.Run()) |> ignore
+    Task.Run(fun () -> app.Run()) |> ignore
 
-        let window = new PhotinoWindow(Title = "Full Stack F# on Desktop (Using Photino)")
+    let window = new PhotinoWindow(Title = "Full Stack F# on Desktop (Using Photino)")
 
-        window
-            .Center()
-            .Load(Uri(desktopUrl))
-            .WaitForClose()
+    window.Center().Load(Uri(desktopUrl)).WaitForClose()
 
-        exitCode
+    exitCode
--- UserInterface/App.fs
+++ UserInterface/App.fs
@@ -4,26 +4,53 @@ open Feliz
 open Feliz.UseDeferred
 open Feliz.Shadcn
 
+
 /// <summary>
 /// A React component that loads the system information from the backend API and shows on screen
 /// </summary>
 [<ReactComponent>]
 let SystemInfo () =
   let data = React.useDeferred (Server.Api.SystemInfo(), [||])
+  let refreshKey, setRefreshKey = React.useState 0
+  let counter = React.useDeferred (Server.Api.Counter(), [| refreshKey |])
+
+  let counterValue =
+    match counter with
+    | Deferred.HasNotStartedYet -> "-"
+    | Deferred.InProgress -> "loading..."
+    | Deferred.Resolved counter -> $"{counter}"
+    | Deferred.Failed _ -> "error"
 
   match data with
   | Deferred.HasNotStartedYet -> Html.none
   | Deferred.InProgress -> Html.h1 "Loading system info"
   | Deferred.Failed error -> Html.span error.Message
   | Deferred.Resolved system ->
-    Html.div [ Html.h1 [ prop.className "scroll-m-20 text-4xl font-extrabold tracking-tight text-balance"
-                         prop.text $"Platform: {system.Platform}" ]
-               Html.h3 [ prop.className "scroll-m-20 text-2xl font-semibold tracking-tight"
-                         prop.text $"Version: {system.Version}"
-                         prop.style [ style.color.mediumAquamarine ] ]
+    Html.div [
+      prop.className "p-4 flex flex-col gap-2"
+      prop.children [
+        Html.h1 [
+          prop.className "scroll-m-20 text-4xl font-extrabold tracking-tight text-balance"
+          prop.text $"Platform: {system.Platform}"
+        ]
+        Html.h3 [
+          prop.className "scroll-m-20 text-2xl font-semibold tracking-tight"
+          prop.text $"Version: {system.Version}"
+          prop.style [ style.color.mediumAquamarine ]
+        ]
 
-               Shadcn.button [ prop.text "Show Alert"
-                               prop.onClick (fun _ -> Browser.Dom.window.alert "Hello, shadcn/ui!") ]
+        Html.p [ Html.span "Counter value: "; Html.text counterValue ]
 
-               Shadcn.button [ prop.onClick (fun _ -> Server.Api.Update() |> Async.Ignore |> Async.StartImmediate)
-                               prop.text "Click me" ] ]
+        Shadcn.button [
+          prop.className "w-fit"
+          prop.onClick (fun _ ->
+            async {
+              do! Server.Api.Update()
+              setRefreshKey (refreshKey + 1)
+            }
+            |> Async.Ignore
+            |> Async.StartImmediate)
+          prop.text "Click me"
+        ]
+      ]
+    ]
