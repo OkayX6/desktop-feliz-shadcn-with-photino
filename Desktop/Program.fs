5ee1c8cb58298c3f6e757f49d3ad0f60c3f77414
Denis Ok <denis@getdefacto.com>
2025-08-26 00:33:04 +0200

    Upgrade lucide-react + remove Desktop Suave

--- Desktop/Program.fs
+++ Desktop/Program.fs
@@ -1,102 +1,92 @@
-﻿module Program
+namespace Desktop.AspNet
+
+#nowarn "20"
 
 open System
-open System.Threading
-
-open Suave
+open System.Threading.Tasks
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Fable.Remoting.AspNetCore
 open Shared
-open Fable.Remoting.Server
-open Fable.Remoting.Suave
-open PhotinoNET
-open System.IO
-open System.Reflection
 
-let serverApi: IServerApi =
-  let mutable count = 0
+module Program =
+  open PhotinoNET
+  open Fable.Remoting.Server
+  open System.Net.Sockets
+  open System.Net
 
-  { Counter = fun () -> async { return count }
+  let serverApi: IServerApi =
+    let mutable count = 0
 
-    SystemInfo =
-      fun () ->
-        async {
-          return
-            { Platform = Environment.OSVersion.Platform.ToString()
-              Version = Environment.OSVersion.VersionString }
-        }
+    { Counter = fun () -> async { return count }
 
-    Update =
-      fun () ->
-        async {
-          count <- count + 1
-          return ()
-        } }
+      SystemInfo =
+        fun () ->
+          async {
+            return
+              { Platform = Environment.OSVersion.Platform.ToString()
+                Version = Environment.OSVersion.VersionString }
+          }
 
-let webApi =
-  Remoting.createApi ()
-  |> Remoting.fromValue serverApi
-  |> Remoting.withRouteBuilder routerPaths
-  |> Remoting.buildWebPart
+      Update =
+        fun () ->
+          async {
+            count <- count + 1
+            return ()
+          } }
 
-let random = Random()
-// TODO: find a better way for choosing a free port
-let randomPort = random.Next(9000, 10000)
+  let exitCode = 0
 
-let webApp = choose [ webApi; Files.browseHome ]
 
-let isDevelopment =
+  let isDevelopment =
 #if DEBUG
-  true
+    true
 #else
-  false
+    false
 #endif
 
-let suavePort =
-  if isDevelopment
-  // Suave web server has to run on port 5000
-  // during development because webpack-dev-server proxies
-  // requests to the backend here
-  then
-    5000
-  else
-    randomPort
+  let randomPort =
+    let tcpListener = new TcpListener(IPAddress.Loopback, 0)
+    tcpListener.Start()
+    let port = (tcpListener.LocalEndpoint :?> IPEndPoint).Port
+    tcpListener.Stop()
+    port
 
-let desktopUrl =
-  if isDevelopment
-  // during development assume webpack dev server is running
-  then
-    "http://localhost:5173"
-  // in release mode we run Suave on a random port
-  // which will host the static files generated
-  else
-    $"http://localhost:{randomPort}/index.html"
+  let desktopUrl =
+    if
+      isDevelopment
+    // during development assume vite dev server is running
+    then
+      "http://localhost:5173"
+    // in release mode we run Suave on a random port
+    // which will host the static files generated
+    else
+      $"http://localhost:{randomPort}/index.html"
 
-[<EntryPoint; STAThread>]
-let main args =
-  let cts = new CancellationTokenSource()
-  let currentAssembly = Assembly.GetExecutingAssembly()
-  let executableDirectory = Directory.GetParent(currentAssembly.Location).FullName
-  let frontendAssets = Path.Combine(executableDirectory, "wwwroot")
+  let webApp =
+    Remoting.createApi ()
+    |> Remoting.fromValue serverApi
+    // It's not in the documentation but if you don't do it, it doesn't work
+    |> Remoting.withRouteBuilder routerPaths
 
-  let suaveConfig =
-    { defaultConfig with
-        homeFolder = Some frontendAssets
-        bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" suavePort ]
-        bufferSize = 2048
-        cancellationToken = cts.Token }
+  [<EntryPoint; STAThread>]
+  let main args =
+    let builder = WebApplication.CreateBuilder(args)
 
-  printfn "Starting up server"
-  let listening, server = startWebServerAsync suaveConfig webApp
-  Async.Start server
+    // Configure the server to run on port 5000
+    builder.WebHost.UseUrls("http://localhost:5000")
 
-  listening |> Async.RunSynchronously |> ignore
+    let app = builder.Build()
 
-  if isDevelopment then
-    printfn "Suave server started"
-    printfn "A Phontino window should pop up"
-    printfn "That window is running the user interface hosted by webpack at http://localhost:5173"
-    printfn "Meanwhile the (Suave) backend is running on http://localhost:5000"
-    printfn "Webpack dev server will proxy all HTTP calls to Suave during the development session"
+    // Add Remoting handler to the ASP.NET Core pipeline
+    app.UseRemoting(webApp)
 
-  let window = new PhotinoWindow(Title = "Full Stack F# on Desktop (Using Photino)")
-  window.Center().Load(Uri(desktopUrl)).WaitForClose()
-  0
+    Task.Run(fun () -> app.Run()) |> ignore
+
+    let window = new PhotinoWindow(Title = "Full Stack F# on Desktop (Using Photino)")
+
+    window.Center().Load(Uri(desktopUrl)).WaitForClose()
+
+    exitCode
