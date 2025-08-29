2b573cbb7563652f024fb08b4759bd3d7221f45c
Denis Ok <denis@getdefacto.com>
2025-08-29 22:46:39 +0200

    More realistic example

--- Desktop/Program.fs
+++ Desktop/Program.fs
@@ -1,4 +1,4 @@
-namespace Desktop
+module Desktop.Program
 
 #nowarn "20"
 
@@ -10,103 +10,105 @@ open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Fable.Remoting.AspNetCore
 open Shared
+open Photino.NET
+open Fable.Remoting.Server
+open System.Net.Sockets
+open System.Net
+open System.Reflection
+open System.IO
 
-module Program =
-  open Photino.NET
-  open Fable.Remoting.Server
-  open System.Net.Sockets
-  open System.Net
-  open System.Reflection
-  open System.IO
+let serverApi: IServerApi =
+  let initialDirectory = 
+    let currentAssembly = Assembly.GetExecutingAssembly()
+    Directory.GetParent(currentAssembly.Location).FullName
+    
+  let mutable currentDirectory = initialDirectory
 
-  let serverApi: IServerApi =
-    let initialDirectory = Assembly.GetExecutingAssembly().Location
-    let mutable currentDirectory = initialDirectory
+  { CurrentFiles =
+      fun () ->
+        async {
+          return
+            { DirectoryPath = currentDirectory
+              Files = Directory.EnumerateFiles(currentDirectory) |> Seq.map Path.GetFileName |> Seq.toArray
+              Directories =
+                Directory.EnumerateDirectories(currentDirectory)
+                |> Seq.map Path.GetFileName
+                |> Seq.toArray }
+        }
+    MoveUp =
+      fun () ->
+        async {
+          let parentDir = Directory.GetParent(currentDirectory)
 
-    { CurrentFiles =
-        fun () ->
-          async {
-            return
-              { DirectoryPath = currentDirectory
-                Files = Directory.EnumerateFiles(currentDirectory) |> Seq.toArray
-                Directories =
-                  Directory.EnumerateDirectories(currentDirectory)
-                  |> Seq.toArray }
-          }
-      MoveUp =
-        fun () ->
-          async {
-            let parentDir = Directory.GetParent(currentDirectory)
+          if parentDir.Exists then
+            currentDirectory <- parentDir.FullName
+        }
+    GoToDirectory =
+      fun (dirPath: string) ->
+        async {
+          if Directory.Exists(dirPath) then
+            currentDirectory <- dirPath
+        }
 
-            if parentDir.Exists then
-              currentDirectory <- parentDir.FullName
-          }
-      GoToDirectory =
-        fun (dirPath: string) ->
-          async {
-            if Directory.Exists(dirPath) then
-              currentDirectory <- dirPath
-          }
+    Reset = fun () -> async { currentDirectory <- initialDirectory } }
 
-      Reset = fun () -> async { currentDirectory <- initialDirectory } }
-
-  let exitCode = 0
+let exitCode = 0
 
 
-  let isDevelopment =
+let isDevelopment =
 #if DEBUG
-    true
+  true
 #else
-    false
+  false
 #endif
 
-  let randomPort =
-    let tcpListener = new TcpListener(IPAddress.Loopback, 0)
-    tcpListener.Start()
-    let port = (tcpListener.LocalEndpoint :?> IPEndPoint).Port
-    tcpListener.Stop()
-    port
+let randomPort =
+  let tcpListener = new TcpListener(IPAddress.Loopback, 0)
+  tcpListener.Start()
+  let port = (tcpListener.LocalEndpoint :?> IPEndPoint).Port
+  tcpListener.Stop()
+  port
 
-  let apiHostUrl =
-    if isDevelopment then
-      "http://localhost:5000"
-    else
-      $"http://localhost:{randomPort}"
+let apiHostUrl =
+  if isDevelopment then
+    "http://localhost:5000"
+  else
+    $"http://localhost:{randomPort}"
 
-  let desktopUrl =
-    if
-      isDevelopment
-    // during development assume vite dev server is running
-    then
-      "http://localhost:5173"
-    // in release mode we run ASP.NET Core on a random port
-    // which will host the static files generated
-    else
-      $"http://localhost:{randomPort}/index.html"
+let desktopUrl =
+  if
+    isDevelopment
+  // during development assume vite dev server is running
+  then
+    "http://localhost:5173"
+  // in release mode we run ASP.NET Core on a random port
+  // which will host the static files generated
+  else
+    $"http://localhost:{randomPort}/index.html"
 
-  let webApp =
-    Remoting.createApi ()
-    |> Remoting.fromValue serverApi
-    // It's not in the documentation but if you don't do it, it doesn't work
-    |> Remoting.withRouteBuilder routerPaths
+let webApp =
+  Remoting.createApi ()
+  |> Remoting.fromValue serverApi
+  // It's not in the documentation but if you don't do it, it doesn't work
+  |> Remoting.withRouteBuilder routerPaths
 
-  [<EntryPoint; STAThread>]
-  let main args =
-    let builder = WebApplication.CreateBuilder(args)
-    // Configure the server hosting URL
-    builder.WebHost.UseUrls(apiHostUrl)
+[<EntryPoint; STAThread>]
+let main args =
+  let builder = WebApplication.CreateBuilder(args)
+  // Configure the server hosting URL
+  builder.WebHost.UseUrls(apiHostUrl)
 
-    let app = builder.Build()
+  let app = builder.Build()
 
-    do
-      app.UseRemoting(webApp) // Add Fable.Remoting handler to the ASP.NET Core pipeline
+  do
+    app.UseRemoting(webApp) // Add Fable.Remoting handler to the ASP.NET Core pipeline
 
-      if not isDevelopment then
-        app.UseStaticFiles() |> ignore // In release mode, serve static files to host the website
+    if not isDevelopment then
+      app.UseStaticFiles() |> ignore // In release mode, serve static files to host the website
 
-      Task.Run(fun () -> app.Run())
+    Task.Run(fun () -> app.Run())
 
-    let window = new PhotinoWindow(Title = "Full Stack F# on Desktop (Using Photino)")
-    window.Center().Load(Uri(desktopUrl)).WaitForClose()
+  let window = new PhotinoWindow(Title = "Full Stack F# on Desktop (Using Photino)")
+  window.Center().Load(Uri(desktopUrl)).WaitForClose()
 
-    exitCode
+  exitCode
